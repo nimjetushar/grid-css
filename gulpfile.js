9b5530c412eb2f26849e0992024d5227925f4063
Tushar Nimje <tusharnimje21@gmail.com>
2022-04-03 16:33:37 +0530

    updated packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require("gulp"),
-  sass = require("gulp-sass"),
+  sass = require("gulp-sass")(require('sass')),
   autoprefixer = require("gulp-autoprefixer"),
   csso = require("gulp-csso"),
   del = require("del"),
